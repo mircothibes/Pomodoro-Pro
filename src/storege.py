47738d19898c83db405da8eb98e9fff3ed7ff8f9
Marcos Vinicius Thibes Kemer <mircothibes@gmail.com>
2025-09-08 14:26:33 +0200

    storege.py

---
+++ src/storege.py
@@ -0,0 +1,9 @@
+DEFAULT_CFG = {
+    "work_sec": 25*60,
+    "short_sec": 5*60,
+    "long_sec": 15*60,
+    "sessions_per_long": 4,
+    "sound": True,
+    "notify": True,
+    "theme": "light",
+}
